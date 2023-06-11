b6a0ef830ef2dabf3e8c77a98d2349b9b8d40e89
shadan khan <kshadan03@gmail.com>
2023-06-11 23:28:31 +0530

    Assignment Vehicle

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
+        mercedes.move(40, 4);
         mercedes.accelerate(20);
-        mercedes.accelerate(-60);
+        mercedes.accelerate(60);
     }
 }
